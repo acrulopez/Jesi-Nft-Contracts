63b8f6f8cb0c61df363ddd036f713afee7b45547
Alex de la Cruz <alex@Alexs-MacBook-Air.local>
2022-05-23 14:07:35 +0200

    added token maximum

--- contracts/JesiArt.sol
+++ contracts/JesiArt.sol
@@ -6,13 +6,19 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract JesiArt is ERC721URIStorage {
     uint256 public tokenCounter;
+    uint256 public maxTokens;
 
-    constructor(string memory _name, string memory _token)
-        ERC721(_name, _token)
-    {}
+    constructor(
+        string memory _name,
+        string memory _token,
+        uint256 _maxTokens
+    ) ERC721(_name, _token) {
+        maxTokens = _maxTokens;
+    }
 
-    function create_ntf(string memory _tokenURI) public {
-        _mint(msg.sender, tokenCounter);
+    function mint(address _nftOwner, string memory _tokenURI) public {
+        require(tokenCounter <= maxTokens);
+        _mint(_nftOwner, tokenCounter);
         _setTokenURI(tokenCounter, _tokenURI);
         tokenCounter = tokenCounter + 1;
     }
